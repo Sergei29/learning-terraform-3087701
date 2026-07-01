8394c02bd78234565fb51cff088ff3e6afb55c76
Serge Basangovs <sergejs.basangovs@gmail.com>
2026-07-01 10:59:47 +0100

    refactor: main tf to use most common ec2 instance type

--- main.tf
+++ main.tf
@@ -1,22 +1,24 @@
+# 1. Fetch the official Canonical Ubuntu 24.04 LTS AMI
 data "aws_ami" "app_ami" {
   most_recent = true
+  owners      = ["099720109477"] # Official Canonical Owner ID for Ubuntu
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    # This grabs the latest x86_64 Ubuntu 24.04 Noble LTS image with GP3 storage
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
+# 2. Provision the Free-Tier Eligible EC2 instance
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "t3.micro" # 750 free hours/month on the standard AWS Free Tier
 
   tags = {
     Name = "HelloWorld"
